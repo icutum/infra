bc2c65ee3c0a55881bfe930c1701caf0a2588848
Mario <icutum@hotmail.com>
2026-03-20 00:20:03 +0100

    feat: create vm module and migrate existing configuration

---
+++ modules/vm/main.tf
@@ -0,0 +1,45 @@
+resource "proxmox_virtual_environment_vm" "this" {
+  name      = var.name
+  node_name = var.node_name
+  vm_id     = var.vm_id
+
+  clone {
+    vm_id = var.template_id
+  }
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = var.cores
+    type  = "x86-64-v2-AES"
+  }
+
+  memory {
+    dedicated = var.memory
+  }
+
+  disk {
+    interface = "scsi0"
+    size      = var.disk
+  }
+
+  network_device {
+    firewall = true
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = var.ip
+        gateway = var.gateway
+      }
+    }
+    
+    user_account {
+      username = "mario"
+      keys     = [var.ssh_public_key]
+    }
+  }
+}
---
+++ modules/vm/variables.tf
@@ -0,0 +1,54 @@
+variable "name" {
+  type        = string
+  description = "Hostname and display name assigned to the virtual machine"
+}
+
+variable "node_name" {
+  type        = string
+  description = "Proxmox node where the virtual machine will be created"
+}
+
+variable "vm_id" {
+  type        = number
+  description = "Unique Proxmox VM identifier (must not conflict with existing VMs)"
+}
+
+variable "template_id" {
+  type        = number
+  description = "VM ID of the Proxmox template used as the cloning source"
+}
+
+variable "cores" {
+  type        = number
+  description = "Number of CPU cores allocated to the virtual machine"
+  default     = 2
+}
+
+variable "memory" {
+  type        = number
+  description = "Amount of RAM allocated to the virtual machine in MB"
+  default     = 2048
+}
+
+variable "disk" {
+  type        = number
+  description = "Disk size for the virtual machine in GB"
+  default     = 32
+}
+
+variable "ip" {
+  type        = string
+  description = "Static IPv4 address assigned to the virtual machine (CIDR notation, e.g. 192.168.1.10/24)"
+  default     = "dhcp"
+}
+
+variable "gateway" {
+  type        = string
+  description = "Default gateway IPv4 address for the virtual machine"
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "SSH public key injected into the VM for remote access (cloud-init)"
+  sensitive   = true
+}
